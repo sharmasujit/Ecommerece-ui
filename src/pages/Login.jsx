97c8807ef448826bef7f91f1fc1ff005d9627a14
Sujit Sharma <shuzeet15@gmail.com>
2024-02-06 15:08:39 +0545

    login completed

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,11 +1,39 @@
-import { Button, FormControl, TextField, Typography } from '@mui/material'
+import { Button, FormControl, FormHelperText, LinearProgress, Stack, TextField, Typography } from '@mui/material'
 import { Formik } from 'formik'
 import React from 'react'
+import { useMutation } from 'react-query'
+import { Link, useNavigate } from 'react-router-dom'
 import * as Yup from "yup"
+import $axios from '../../lib/axios.instance'
+import { useDispatch } from 'react-redux'
+import { openErrorSnackbar, openSuccessSnackbar } from '../store/slices/snackbar.slices'
 
 const Login = () => {
+  const dispatch=useDispatch();
+  const navigate=useNavigate();
+
+  const {isLoading,mutate:loginUser}=useMutation({
+    mutationKey:["user-login"],
+    mutationFn:async(values)=>{
+      return await $axios.post("/user/login",values);
+    },
+
+    onSuccess:(response)=>{
+      dispatch(openSuccessSnackbar("Login Successfully"));
+      navigate("/");
+
+      localStorage.setItem("token",response?.data?.token);
+      localStorage.setItem("firstName",response?.data?.user?.firstName);
+      localStorage.setItem("role",response?.data?.user?.role);
+    },
+
+    onError:(error)=>{
+      dispatch(openErrorSnackbar(error?.response?.data?.message))
+    }
+  })
   return (
     <>
+    {isLoading && <LinearProgress color='secondary'/>}
     <Formik
     initialValues={
       {
@@ -19,7 +47,7 @@ const Login = () => {
     })}
 
     onSubmit={(values)=>{
-      console.log(values);
+      loginUser(values);
     }}
     >
       {({handleSubmit,getFieldProps,errors,touched})=>(
@@ -36,25 +64,24 @@ const Login = () => {
         >
           <Typography variant='h4'>Sign in</Typography>
           <FormControl>
-            <TextField 
-            color='success'
-            label="Email" variant="outlined" 
-            error={!!touched.email && !!errors.email}
-            helperText={errors.email}
-            {...getFieldProps("email")}
-          />
+            <TextField label="Email" color='success' variant="outlined" {...getFieldProps("email")}/>
+          {touched.email && errors.email ? 
+          (<FormHelperText error>{errors.email}</FormHelperText>) : null}
           </FormControl>
           
           <FormControl>
-            <TextField 
-            color='success'
-            error={!!touched.password && !!errors.password}
-            helperText={errors.password}
-            label="Password" variant="outlined" 
-            {...getFieldProps("password")}
-          />
+            <TextField label="Password" color='success' variant="outlined" {...getFieldProps("password")}/>
+          {touched.password && errors.password ? 
+          (<FormHelperText error>{errors.password}</FormHelperText>) : null}
           </FormControl>
-          <Button variant='contained' type='submit' color='success'>Log In</Button>
+
+          <Stack spacing={1}>
+            <Button variant='contained' type='submit' color='success' disabled={isLoading} >Log In</Button>
+
+            <Link to='/register'>
+              <Typography variant='subtitle2' color="info">New here? Register</Typography>
+            </Link>
+          </Stack>
         </form>
       )}
 
